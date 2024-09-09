b35d78209f8ef7a65c1dcf3779bd053d0a8a7916
DanielRepp <daniel.g.repp@gmail.com>
2024-09-09 13:46:24 +0200

    Add files via upload

--- fitPowerCurve_geneticAlgorithm_github/main.cpp
+++ fitPowerCurve_geneticAlgorithm_github/main.cpp
@@ -1,70 +1,123 @@
-#include "domainHandler.h"
-#include "entity.h"
-#include "material.h"
-#include "position.h"
-#include "source.h"
-#include "fdtd.h"
 #include <iostream>
-#include <math.h>
-#include "monitor.h"
-#include <cstdio>
+#include <vector>
+#include <fstream>
+#include <sstream>
 #include <string>
+#include <mpi.h>
+#include "calculator.h"
+#include "optimizer_evolve.h"
+#include "model.h"
+#include <float.h>
+using namespace std;
 
-int main()
-{
-
-    double radiusWire = 0.1;
-    double spacerWidth = 0.1;
-    double metalWidth = 0.1;
-    double Lx = 2;
-    double Ly = 1;
-    double dx = 0.001;
-    double C = 0.5;
-    double dt = C*dx;  
-    double T = 5/3.33;
-    int sample = 10;
-
-    int numberOfPoints = 11;
-    double width = 0.1;
-    double centerWavelength = 0.38;
-
-    domainHandler dh(Lx, Ly, dx, dx, dt);
-    ellipse wire(radiusWire, radiusWire);
-    rectangle spacer(Lx, spacerWidth);
-    position pSource = position(0,0.1);
-    position pWire = position(0,-0.1);
-    position pSpacer = position(0,-0.1-radiusWire-spacerWidth/2);
-
-
-    material Al = material("Al",1,1,0,0);
-    Al.adddDrudePole(3.33E-15*2*3.1415*3800E12, 3.33E-15*1E15);
+double T = 100E-9;
+double g = 700000;
+double n_trans = 2e+24;
+double tau1 = 10E-9;
+double P1 = 4;
+double T1 = 7E-9;
+double d = 200*1E-9;
+double L = 6E-6;
+double tau_nonrad = 700E-12;
+double tau_rad = 700E-12;
+double F =	45.8;
+double G = 3.4;
+double f_p = 1.42968e+13;
+double beta = 0.94;
+double A = 8.57383e-14;
+double n_abs = 5.61131e+37;
+double A_auger = 1E-57;
+vector<double> paramsStart{T, g, n_trans, tau1, P1, T1, d, L, tau_nonrad, tau_rad, F, G, f_p, beta, A, n_abs, A_auger};
 
 
-    //cout << "Placing Wire." << endl;
-    //dh.addEntity(wire, pWire, material("ZnO",6.5,1,0,0), false, true);
-    //cout << "Placing Spacer." << endl;
-    //dh.addEntity(spacer, pWire, Al, false,true);
-    //cout << "Creating dipole source." << endl;
-    //dipoleSource s(Lx, Ly, dx, dx, p1, omega, orientation, amplitude, T/100);
-    dh.addPML(30,0.2);
-    //dh.printDomain();
+vector<vector<double> > readData(string filename) {
+    vector<vector<double> > data;
+	vector<double> row;
+	string line, word;
 
-    string direction = "x";
-    double length = 1;
-    double omega = 2*M_PI/0.38;
-    double amplitude = 1;
-    vector<int> orientation{0,0,1,1};
-    gaussianSource s(Lx, Ly, dx, dx, pSource, length, direction, omega, orientation, amplitude, 10*T, 0.4, 0.1);
-    //lineSource s(Lx, Ly, dx, dx, pSource, length, direction, omega, orientation, amplitude, 10*T);
+    fstream file (filename, ios::in);
+	if(file.is_open())
+	{
+		while(getline(file, line))
+		{
+			row.clear();
+ 
+			stringstream str(line);
+ 
+			while(getline(str, word, ' ')) {
+				row.push_back(stod(word));
+			}
+			data.push_back(row);
+		}
+	}
+	else
+		cout<<"Could not open the file\n";
 
-    cout << "Creating monitor." << endl;
-    freopen( "I_planeWave.csv", "w", stderr );
-    monitor m(Lx, Ly); 
+	return data;
+}
 
-    cout << "Creating fdtd."<< endl;
-    fdtd f(dh, dt, T, sample, &s, &m);
 
-    f.run();
-    
-    return 0;
+int main() {
+	MPI_Init(NULL, NULL);
+	int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+	vector<vector<double> > data = readData("/beegfs/ni86did/fitPowerCurve_geneticAlgorithm/Data_NW4_onAg_pumpPERP_S5.txt");
+
+	int N_generations = 20000;
+
+	if (world_rank == 0) {
+		int numberOfSurvivors = 60;
+		int numberToPrint = 10;
+		double mutationBound = 2;	
+		vector<int> dontVary{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 14, 16};
+		double stepLength = 0.1;
+		double learningrate = 0.1;
+		optimizer_evolve oe(model::Nparameters, world_size-1, numberOfSurvivors, mutationBound, paramsStart, data, T, stepLength, learningrate);
+		vector<int> indBounded{13}; vector<double> low{0.1}; vector<double> high{0.99};
+		oe.setBounds(indBounded, low, high);
+		oe.set_dontVary(dontVary);
+		oe.initialize();
+		for (int n=0; n<N_generations; n++) {
+			vector<vector<double>> allParams = oe.getAllParameters();
+			for (int k=1; k<world_size;k++) {
+				vector<double> sendLine = allParams[k-1];
+				MPI_Send(&sendLine[0], model::Nparameters, MPI_DOUBLE, k, n, MPI_COMM_WORLD);
+			}
+			vector<double> allErrors(world_size-1,0.0);
+			for (int k=1; k<world_size;k++) {
+				MPI_Recv(&allErrors[k-1], 1, MPI_DOUBLE, k, n, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			}
+			oe.set_Errors(allErrors);
+			double lowestError = oe.getLowestError();
+			cout << "Lowest error " << lowestError << endl;
+			double largestError = oe.getLargestError();
+			cout << "Largest error " << largestError << endl;
+			if (n%100 == 99) oe.printSurvivors(numberToPrint);
+			if (lowestError < DBL_MAX) {
+				oe.setMutationBound(lowestError);
+				oe.removeUnfit();
+				oe.generateChildren();
+				oe.enforceBounds();
+			} else {
+				oe.initialize();
+			}
+		}
+		oe.printSurvivors(numberOfSurvivors);
+	} else  {
+		calculator c(world_rank, data, T);
+		vector<double> params = vector<double>(model::Nparameters,0.0);
+		for(int n=0; n<N_generations; n++) {
+			MPI_Recv(&params[0], model::Nparameters, MPI_DOUBLE, 0, n,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			c.setParameters(params);
+			double error = c.run(false);
+			MPI_Send(&error, 1, MPI_DOUBLE, 0, n, MPI_COMM_WORLD);
+		}
+	}
+
+	
+    MPI_Finalize();
+	return 0;
 }
